27a98a8635fc674561e50bd4532a6e6a7c6cc476
Sabri Guvendi <guvendisabri@gmail.com>
2022-05-30 00:19:21 +0300

    Java Oncesi Practice1

---
+++ src/kisiselKullanim/JavaHazirlikPractice1.java
@@ -0,0 +1,28 @@
+package kisiselKullanim;
+
+public class JavaHazirlikPractice1 {
+
+    public static void main(String[] args) {
+
+        double doublemin= Double.MIN_VALUE;
+        System.out.println("doublemin = " + doublemin);
+
+
+        double doubleMax=Double.MAX_VALUE;
+        System.out.println("doubleMax = " + doubleMax);
+
+
+        float floatMin= Float.MIN_VALUE;
+        System.out.println("floatMin = " + floatMin);
+
+        float floatMax=Float.MAX_VALUE;
+        System.out.println("floatMax = " + floatMax);
+
+        byte bytemin=Byte.MIN_VALUE;
+        System.out.println("bytemin = " + bytemin);
+
+        byte bytemax=Byte.MAX_VALUE;
+        System.out.println("bytemax = " + bytemax);
+
+    }
+}
