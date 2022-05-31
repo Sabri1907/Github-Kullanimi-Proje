807f0a87a0201d0be315a09cc4700ac1e93aa7bc
Sabri Guvendi <guvendisabri@gmail.com>
2022-05-31 03:24:06 +0300

    31.05.2022

---
+++ src/javaOnHazirlik/Practice1.java
@@ -0,0 +1,8 @@
+package javaOnHazirlik;
+
+public class Practice1 {
+
+    public static void main(String[] args) {
+        System.out.println("\"Zaruret\" insani \nkasif yapar \n\n\n\t\"TechProEd ile java cok kolay...\"");
+    }
+}
