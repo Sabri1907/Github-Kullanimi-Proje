4c8455ded9fa4420fe8703bcc667e40986eeaf54
Sabri Guvendi <guvendisabri@gmail.com>
2022-06-17 00:17:13 +0300

    17.06.2022

--- src/javaOnHazirlik/Q17.java
+++ src/javaOnHazirlik/Q17.java
@@ -9,21 +9,21 @@ public class Q17 {
         float sayi4=10000f;
         double sayi5=1000000.60;
 
-        /*
+
         sayi5=sayi4;
         System.out.println(sayi5); //10000.0
-        */
 
-        /*
+
+
         sayi4=sayi3;
         System.out.println(sayi4); //1000.0
-        */
 
 
-        /*
+
+
         sayi3=sayi2;
         System.out.println(sayi2); //100
-        */
+        
 
         sayi2=sayi1;
         System.out.println(sayi1);//10
